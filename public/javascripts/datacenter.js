7140c448a2ce24b9c8e488c7c1224fa39da336e3
Amjad Ali <amjad.4@gmail.com>
2016-10-07 01:12:29 +0530

    Remove redundant javascript files, update menu

--- public/javascripts/datacenter.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  	var socket = io();
- 	io.on('connection', function(socket){
-		console.log("Connected");
-		socket.on('chat message', function(msg){
-			$('#messages').append($('<li>').text(msg));
-		});
-	});
-});
