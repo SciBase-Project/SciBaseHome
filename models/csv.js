841437773f297d6824ac8d47f2157ba87248ac5a
Harsha Vamsi <harshavamsi096@gmail.com>
2017-03-13 21:44:30 +0530

    Fixed test setup and mongodb issues

--- models/csv.js
+++ models/csv.js
@@ -1,6 +1,3 @@
  var mongoose = require('mongoose');
-var updateCsv = new mongoose.Schema({
-    name : String,
-	update_next: { type: Date, default : Date.now },
-});
+
 module.exports = mongoose.model('updateCsv', updateCsv); 
