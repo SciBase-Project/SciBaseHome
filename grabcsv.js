7fbe5308ff1ae67d82abf9119a134bc0ee8e66e2
Harsha Vamsi <harshavamsi096@gmail.com>
2017-03-13 22:21:34 +0530

    Added script to grab csv's

---
+++ grabcsv.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var csv = require("./models/csv")
+try {
+  mongoose.connect('mongodb://localhost/scibase');
+} catch (err) {
+  throw new Error("Could not connect to database");
+}
+csv.findOneAndRemove({ "name" :'update' },function(err,doc,result){
+    if(err)
+    console.log(err)
+    else {
+        console.log("Removed date from mongodb");
+        return -1;
+    }
+
+})
--- models/csv.js
+++ models/csv.js
@@ -1,3 +1,9 @@
  var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+ var updateCsv = new schema({
+     name : String,
+     update_next: { type: Date, default : Date.now },
+ },{collection : "counts"});
 
-module.exports = mongoose.model('updateCsv', updateCsv); 
+ var updateCsvModel = mongoose.model("updateCsvModel", updateCsv);
+module.exports = updateCsvModel
